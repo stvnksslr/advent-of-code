dbd9e18ed2ee0cae654947c5eef15aa4b34f303c
Steven kessler <stvnksslr@gmail.com>
2018-12-17 22:51:18 -0500

    feat(first pair of sums): none

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ current_frequency = 0
 list_of_frequencies = day1_constants.TEST_LIST
 list_of_sums = []
 
-for frequency in list_of_frequencies:
-    current_frequency = frequency + current_frequency
-    list_of_sums.append(current_frequency)
-    # list_of_sums = list_of_sums.append(current_frequency)
+duplicate_checker = True
 
-first_pair = day1.Day1.find_duplicates(list_of_sums)
-print(first_pair)
+while duplicate_checker:
+    for frequency in list_of_frequencies:
+        current_frequency = frequency + current_frequency
+        list_of_sums.append(current_frequency)
+    first_pair = day1.Day1.find_duplicates(list_of_sums)
+    if first_pair:
+        print(first_pair)
+        duplicate_checker = False
+        break
+    
