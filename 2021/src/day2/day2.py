7b46627d9e88675d5b4c32dc5b2266483fe4d090
Steven Kessler <stvnksslr@gmail.com>
2021-12-02 23:28:45 -0800

    2021 day2 pt2 finished

--- 2021/src/day2/day2.py
+++ 2021/src/day2/day2.py
@@ -20,11 +20,10 @@ movement_instructions = [
 def calculate_instructions(input_list):
     starting_horizontal = 0
     starting_vertical = 0
+
     for instruction in input_list:
         if instruction[0] == 'forward':
             starting_horizontal = starting_horizontal + int(instruction[1])
-        if instruction[0] == 'backward':
-            starting_horizontal = starting_horizontal - int(instruction[1])
         if instruction[0] == 'up':
             starting_vertical = starting_vertical - int(instruction[1])
         if instruction[0] == 'down':
@@ -35,9 +34,36 @@ def calculate_instructions(input_list):
     return starting_horizontal * starting_vertical
 
 
+def calculate_instructions_with_aim(input_list):
+    aim = 0
+    horizontal_position = 0
+    vertical_position = 0
+
+    for instruction in input_list:
+        # movement
+        if instruction[0] == 'forward':
+            horizontal_position = horizontal_position + int(instruction[1])
+            vertical_position = vertical_position + (aim * int(instruction[1]))
+
+        # aim adjustments
+        if instruction[0] == 'up':
+            aim = aim - int(instruction[1])
+        if instruction[0] == 'down':
+            aim = aim + int(instruction[1])
+
+    print(f'starting_horizontal: {horizontal_position}')
+    print(f'starting_vertical: {vertical_position}')
+    return horizontal_position * vertical_position
+
+
 def day2_1():
     print(calculate_instructions(PUZZLE_INPUT))
 
 
+def day2_2():
+    print(calculate_instructions_with_aim(PUZZLE_INPUT))
+
+
 if __name__ == '__main__':
     day2_1()  # 2102357
+    day2_2()  # 2101031224
